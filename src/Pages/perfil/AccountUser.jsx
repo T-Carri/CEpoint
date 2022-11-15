85dc4bc0b663621e6428e8e3f09337503742dee8
T-Carri <bryan_narutito@hormail.com>
2022-11-15 16:09:16 -0600

    branch15112022-15112022

--- src/Pages/perfil/AccountUser.jsx
+++ src/Pages/perfil/AccountUser.jsx
@@ -64,12 +64,10 @@ accessKey(),
       };
   
   return (
-
-    
-    <section className="cepoint">
-<div className="Bienvenida">
+<div>
+<header> 
     {/* //navbar */}
-          <nav className="navbar navbar-expand-lg bg-warning w-100 p-4 d-inline-block">
+    <nav className="navbar navbar-expand-lg bg-warning w-100 p-4 d-inline-block">
           <NavbarBrand className="logo" href="/">
             <strong>
            CEpoint
@@ -82,70 +80,68 @@ accessKey(),
        
      </div>
           </nav>
+</header>
+<div className="a1">
+
+    
+{Usator &&<Button variant='warning' className="btnx1" onClick={()=>{navigate("usuarios")}}>
+ <strong>Usuarios
+   </strong></Button>}
+
+{lectorAsistencia && <Button className="btnx3" onClick={handleHorario}> <strong>Asistencia</strong></Button>}    
+{asignador &&<Button variant='danger' className="btnx3" onClick={()=>{navigate("asignadorEndiseño")}}>
+<strong> Asignador  </strong> </Button>}
+
+<Button className="btnx1" variant="success" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Trabaja en otras funciones</Button>
+
 
 
 </div>
-    {/* aside */}
-    <div className="a1">
 
-    
-     {Usator &&<Button variant='warning' className="btnx1" onClick={()=>{navigate("usuarios")}}>
-      <strong>Usuarios
-        </strong></Button>}
-    
-     {lectorAsistencia && <Button className="btnx3" onClick={handleHorario}> <strong>Asistencia</strong></Button>}    
-     {asignador &&<Button variant='danger' className="btnx3" onClick={()=>{navigate("asignadorEndiseño")}}>
-     <strong> Asignador  </strong> </Button>}
-   
-     <Button className="btnx1" variant="success" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Trabaja en otras funciones</Button>
-    
-    
-    
-    </div>
-   
-      {/* //area de herramienta  */}
-    <div className="a21"  >  
-    
+ {/* //area de herramienta  */}
+<div className="a21"  >  
 
- <Outlet/>
-    
-     </div>
-     
-     
-      {/* //canvas lateral */}
-     <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel" >
-          
-    
-          <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Trabaja en las otras funciones</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-    
-    
-    
-    
-        <p>
-          Falta crear:
-        
-          -CONTROL DE ALMACEN POR Qr (APP)
-          -INTERFAZ DE ASISTENCIA RECIBIDO EN "JSON (y)"
-                   tecnologias analizado: Ajax y axios para actualizar y detectar cuando recibo datos. 
-                    
-          -CONVERTIDOR DE JSON A STRIN Y VICEVERSA 
-          -INTEGRAR SOCKET.IO
-          -SE TENDRA QUE HACER UN SERVER BACK END POSIBLEMENTE 
-          CONECTAR AXIOS (CEpoint) CON EXPRESS (server)
-    
-           .</p>
-      </div>
-    
-    
-    
-    
+
+<Outlet/>
+
 </div>
-    </section>
+
+
+ {/* //canvas lateral */}
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel" >
+     
+
+     <div class="offcanvas-header">
+   <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Trabaja en las otras funciones</h5>
+   <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+ </div>
+ <div class="offcanvas-body">
+
+
+
+
+   <p>
+     Falta crear:
+   
+     -CONTROL DE ALMACEN POR Qr (APP)
+     -INTERFAZ DE ASISTENCIA RECIBIDO EN "JSON (y)"
+              tecnologias analizado: Ajax y axios para actualizar y detectar cuando recibo datos. 
+               
+     -CONVERTIDOR DE JSON A STRIN Y VICEVERSA 
+     -INTEGRAR SOCKET.IO
+     -SE TENDRA QUE HACER UN SERVER BACK END POSIBLEMENTE 
+     CONECTAR AXIOS (CEpoint) CON EXPRESS (server)
+
+      .</p>
+ </div>
+
+
+
+
+</div>
+</div>
     
+
     
     
    
