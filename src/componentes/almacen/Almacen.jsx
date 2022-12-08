ceb768e69b6baec64a3d690515c00f26d428094a
T-Carri <bryan_narutito@hormail.com>
2022-12-08 16:56:00 -0600

    8122022-t.1

--- src/componentes/almacen/Almacen.jsx
+++ src/componentes/almacen/Almacen.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
-
+import { Container, Row, Col } from 'react-bootstrap'
+import './almacen.css'
 export const Almacen = () => {
   return (
-    <div>Almacen  el almacen necesita un area de agregar producto o herramienta 
-        area de totales y area de prestado 
-        historial de miscelaneos  <br/>
-        piensa en como crear la base de datos
-    </div>
+    <Container>
+<div className='prestado'>
+  <div className="capaPrestado">
+  <h3>Prestado</h3>
+    <p>Agrega prestado</p> 
+
+  </div>
+</div>
+
+<div className='areas'>
+<div className='miscelaneos'>
+  <div className="capaMiscelaneos">
+    <h3>Miscelaneos</h3>
+    <p>Agrega miscelaneos</p>
+</div>
+</div>
+<div className='maquinaria'>
+  <div className="capaMaquinaria">
+  <h3>Maquinaria</h3>
+    <p>Agrega maquinaria</p>
+
+  </div>
+</div>
+<div className='herramientas'>
+  <div className="capaHerramientas">
+  <h3>Herramientas</h3>
+    <p>Agrega herramientas</p>
+  </div>
+</div>
+</div>
+        
+    </Container>
   )
 }
+
+
+/*
+*/
