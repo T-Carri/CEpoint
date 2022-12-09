bfed7e8f4bee94991075b45b9f907a081d8c9842
T-Carri <bryan_narutito@hormail.com>
2022-12-09 16:51:42 -0600

    9122022

--- src/componentes/almacen/Almacen.jsx
+++ src/componentes/almacen/Almacen.jsx
@@ -1,44 +1,56 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import './almacen.css'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Outlet } from 'react-router-dom'
 export const Almacen = () => {
   const navigate = useNavigate(); 
-  return (
-    <Container>
-<div className='prestado'>
-  <div className="capaPrestado">
-  <h2>Prestado</h2>
-    <p>Agrega prestado</p> 
+const [Toggle, setToggle]=useState(false)
 
-  </div>
+  return (
+
+   
+
+
+Toggle
+?<Outlet/>
+: <Container><div className='prestado'>
+<div className="capaPrestado">
+<h2>Prestado</h2>
+  <p>Agrega prestado</p> 
+
+</div>
 </div>
 
 <div className='areas'>
 <div className='miscelaneos' onClick={()=>{
+setToggle(true)
+navigate("miscelaneos")
+}} >
 
-}}>
-  <div className="capaMiscelaneos">
-    <h2>Miscelaneos</h2>
-    <p>Agrega miscelaneos</p>
+<div className="capaMiscelaneos">
+  <h2>Miscelaneos</h2>
+  <p>Agrega miscelaneos</p>
 </div>
 </div>
 <div className='maquinaria'>
-  <div className="capaMaquinaria">
-  <h2>Maquinaria</h2>
-    <p>Agrega maquinaria</p>
+<div className="capaMaquinaria">
+<h2>Maquinaria</h2>
+  <p>Agrega maquinaria</p>
 
-  </div>
+</div>
 </div>
 <div className='herramientas'>
-  <div className="capaHerramientas">
-  <h2>Herramientas</h2>
-    <p>Agrega herramientas</p>
-  </div>
+<div className="capaHerramientas">
+<h2>Herramientas</h2>
+  <p>Agrega herramientas</p>
 </div>
 </div>
+</div> </Container>
+
+
+
         
-    </Container>
+   
   )
 }
 
